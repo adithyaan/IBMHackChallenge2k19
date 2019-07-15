94a9ca5d2418c4c4846baa78ae65a3845b1b8f6e
adithyaan <anirudhadi777@gmail.com>
2019-07-15 11:07:14 +0530

    retrieve data

--- src/usercreated/Home.js
+++ src/usercreated/Home.js
@@ -4,14 +4,21 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
-
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import Card from '@material-ui/core/Card';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Checkbox from '@material-ui/core/Checkbox';
+import Avatar from '@material-ui/core/Avatar';
 class Home extends Component{
 
 
 
     constructor(props){
         super(props)
-        this.state = {textInput:""};
+        this.state = {textInput:"",search_results:[]};
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
@@ -20,7 +27,20 @@ class Home extends Component{
     };
 
     handleSubmit(event) {
-        alert(this.state.textInput);
+        var url1="https://api.stackexchange.com//2.2/search/advanced?order=desc&sort=activity&";
+        var url2=this.state.textInput+"&";
+        var url3="site=stackoverflow";
+        var finalurl=url1+url2+url3;
+
+        // alert(finalurl);
+        var promise=fetch(finalurl)
+        var result=promise.then((response)=>response.json());
+        result.then((response)=>{
+              this.setState({search_results:response,show:true});
+              alert(this.state.search_results);
+
+          });
+
     }
 
     render(){
@@ -41,6 +61,75 @@ class Home extends Component{
                         <Button color="inherit" onClick={this.handleSubmit}>Search</Button>
                     </Toolbar>
                 </AppBar>
+                <div>
+              {this.state.show && 
+                 <List dense style={styles.container} disablePadding={true}>
+          {this.state.search_results.map((value) => (
+           
+              <Card style={styles.listitem}>
+            <ListItem key={value} button > 
+              <ListItemAvatar>
+                <Avatar
+                  alt={`Avatar n°${value}`}
+                  src="https://cdn3.iconfinder.com/data/icons/wpzoom-developer-icon-set/500/38-512.png"
+                  
+                />
+              </ListItemAvatar>
+          <ListItemText primary={<b>{(value.name).toUpperCase()}</b>  }  secondary={
+            <React.Fragment>
+              <Typography component="span" color="textPrimary">
+                      {value.description}
+              </Typography>
+            </React.Fragment>
+          }/>
+              
+            </ListItem>
+            <ListItem key={value} button > 
+              <ListItemAvatar>
+                <Avatar
+                  alt={`Avatar n°${value}`}
+                  src="https://cdn4.iconfinder.com/data/icons/rcons-user/32/secret_agent_man-512.png"
+                  
+                />
+              </ListItemAvatar>
+          <ListItemText primary={<b>{(value.casts).toUpperCase()}</b>  }  secondary={
+            <React.Fragment>
+              <Typography component="span" color="textPrimary">
+                      {value.releasedon}
+              </Typography>
+            </React.Fragment>
+          }/>
+              
+            </ListItem>
+{/* 
+            <ListItem key={value} button > 
+              <ListItemAvatar>
+                <Avatar
+                  alt={`Avatar n°${value}`}
+                  src="https://cdn3.iconfinder.com/data/icons/main-ui-elements-with-color-bg-vol-2/512/heart-512.png"
+                />
+              </ListItemAvatar>
+          <ListItemText primary={<b>{(value.casts).toUpperCase()}</b>  }  secondary={
+            <React.Fragment>
+              <Typography component="span" color="textPrimary">
+                      {value.releasedon}
+              </Typography>
+            </React.Fragment>
+          }/>
+            </ListItem> */}
+            <ListItem style={{display: 'flex',flex: 1,alignItems:'flex-end'}}>
+            <Avatar 
+                              alt={`Avatar n°${value}`}
+
+            src="https://cdn4.iconfinder.com/data/icons/basic-ui-2-line/32/heart-love-like-likes-loved-favorite-512.png"/>
+            
+            </ListItem>
+            </Card>
+
+          ))}
+        </List>
+              }
+            </div>
             </div>
             
         )
@@ -48,4 +137,17 @@ class Home extends Component{
 
 }
 
+const styles = {
+    container:{
+        position:'absolute',
+        left:'20%',
+        right:'20%',
+        top:'20%',
+          },
+    listitem:{
+        marginBottom:30,
+        width:800,
+        minHeight:300
+    }
+  }
 export default Home;
